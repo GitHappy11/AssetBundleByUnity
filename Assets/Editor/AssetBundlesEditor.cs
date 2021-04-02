ac08df616ce0c08fbc87f9bbb00b9ccaaca2c88d
Happy11 <1056972599@qq.com>
2021-04-03 01:18:32 +0800

    创建AB包-本地及URl加载-编辑器窗口弹窗

---
+++ Assets/Editor/AssetBundlesEditor.cs
@@ -0,0 +1,38 @@
+﻿using UnityEditor;
+using System.IO;
+using UnityEngine;
+public class AssetBundlesEditor
+{
+
+   static string outPath = "AssetBundles";
+
+    [MenuItem("Tools/BuildAssetBundles")]
+   static void BuildAllAssetBundles()
+    {
+        BuildAssetBundles(outPath,BuildAssetBundleOptions.None,BuildTarget.StandaloneWindows64);
+    }
+
+    // 打包AssetBundles 如果将预制体和依赖的材质打包在不同包里，则预制体会丢失材质 ，如果打包在相同的包中则不会丢失
+    private static void BuildAssetBundles(string path, BuildAssetBundleOptions options, BuildTarget platform)
+    {
+        // 提示信息
+        string msg = "当前平台：{0}\n输出路径：{1}";
+        //补充提示信息
+        msg = string.Format(msg, platform.ToString(), path);
+        //创建提示窗口
+        if (EditorUtility.DisplayDialog("打包信息", msg, "确定", "取消"))
+        {
+            //检测路径是否存在，如果不不存在则创建
+            if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+            //打包进度条
+            //EditorUtility.DisplayProgressBar("信息", "正在打包资源包", 0f);
+            //开始打包
+            BuildPipeline.BuildAssetBundles(path, options, platform);
+            //刷新编辑器资源
+            AssetDatabase.Refresh();
+            //tip弹窗提示
+            EditorUtility.DisplayDialog("提示", "打包AssetBundle完毕", "确定");
+        }
+    }
+
+}
---
+++ Assets/Scripts/LoadAssetBundle.cs
@@ -0,0 +1,115 @@
+﻿
+using System.Collections;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Networking;
+using System;
+
+public class LoadAssetBundle : MonoBehaviour
+{
+
+    private string cubePath = "AssetBundles/prefabs/cube.ab";
+
+
+    private void Start()
+    {
+        //以下都是依赖于本地文件的加载方式，还有许多方式，具体查看AssetBundle.LoadFrom系列方法，这里不再做演示
+
+        //LoadFromFileByAB(cubePath);
+        //StartCoroutine("LoadFromMemoryAsyncByAB", cubePath);
+        //LoadFromMemoryByAB(cubePath);
+
+
+        //网络加载 WWW已经被UnityWebRequest代替，所以不再演示
+        //将路径转化为Uri格式，具体内容可调试查看 
+        //var uri = new Uri(@"D:\WorkData\UnityProject\AssetBundleStudy\AssetBundles\prefabs\cube.ab");
+        //StartCoroutine("UnityWebRequestByAB",uri);
+
+        //检查包以及获取包的依赖
+        RelyONByAB("cube.ab");
+
+        LoadFromFileByAB(cubePath);
+
+        //关于卸载资源  当场景切换的时候就需要卸载一次资源，减少内存使用以及防止丢失
+        //卸载所有资源，即便资源在使用
+        //AssetBundle.UnloadAllAssetBundles(true);
+        //卸载个别资源,即便资源在使用
+        //Resources.UnloadAsset();
+        //卸载没有在使用的资源
+        //AssetBundle.UnloadAllAssetBundles(false);
+
+
+    }
+
+    //加载本地文件
+    public void LoadFromFileByAB(string path)
+    {
+       //加载AB包
+       AssetBundle ab= AssetBundle.LoadFromFile(path);
+       //加载GameObject对象
+       GameObject obj = ab.LoadAsset<GameObject>("Cube");
+       Instantiate(obj);
+    }
+
+    //从内存中加载 异步加载
+    IEnumerator  LoadFromMemoryAsyncByAB(string path)
+    {
+        AssetBundleCreateRequest request = AssetBundle.LoadFromMemoryAsync(File.ReadAllBytes(path));
+        //等待加载完成
+        yield return request;
+        //加载完毕后就可以使用了
+        AssetBundle ab = request.assetBundle;
+        //加载GameObject对象
+        GameObject obj = ab.LoadAsset<GameObject>("Cube");
+        Instantiate(obj);
+    }
+
+    //从内存中加载 同步加载
+    private void LoadFromMemoryByAB(string path)
+    {
+        //加载完毕后就可以使用了
+        AssetBundle ab = AssetBundle.LoadFromMemory(File.ReadAllBytes(path));
+        //加载GameObject对象
+        GameObject obj = ab.LoadAsset<GameObject>("Cube");
+        Instantiate(obj);
+    }
+
+    //本地文件路径或网络路径加载
+    IEnumerator UnityWebRequestByAB(Uri uri)
+    {
+        //获取资源
+        UnityWebRequest request = UnityWebRequestAssetBundle.GetAssetBundle(uri);
+        //等待与服务器通信
+        yield return request.SendWebRequest();
+        //取得内容
+        //AssetBundle ab = DownloadHandlerAssetBundle.GetContent(request);
+        //可以下载固定对象
+        AssetBundle ab =(request.downloadHandler as DownloadHandlerAssetBundle).assetBundle;
+        //保存到本地 需要路径 
+        //File.WriteAllBytes();
+
+        //加载GameObject对象 还有多种方式 具体查看文档
+        GameObject obj = ab.LoadAsset<GameObject>("Cube");
+        Instantiate(obj);
+    }
+
+    //查看某个包依赖于哪些包
+    private void RelyONByAB(string abName)
+    {
+        AssetBundle manifestAB = AssetBundle.LoadFromFile("AssetBundles/AssetBundles");
+        AssetBundleManifest manifest = manifestAB.LoadAsset<AssetBundleManifest>("AssetBundleManifest");
+        //输出所有AB包名
+        //foreach (string name in manifest.GetAllAssetBundles())
+        //{
+        //    Debug.Log(name);
+        //}
+
+        //查询某个包的依赖包 并加载
+        string[] strs= manifest.GetAllDependencies(abName);
+        foreach (string name in strs)
+        {
+            Debug.Log(name);
+            AssetBundle.LoadFromFile("AssetBundles/" + name);
+        }
+    }
+}
